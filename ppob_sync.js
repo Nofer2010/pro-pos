eb4544d520b568755bb8051a2e817ef242f0be2d
Ferdy_fernando <ferdianocktavianto90@gmail.com>
2026-09-04 10:19:50 +0700

    ppob_sync.js

--- ppob_sync.js
+++ ppob_sync.js
@@ -3,10 +3,14 @@ const { getFirestore } = require('firebase-admin/firestore');
 const crypto = require('crypto');
 const fetch = require('node-fetch');
 
-// Inisialisasi Firebase Admin jika belum terinisialisasi
+// Inisialisasi Firebase Admin menggunakan variabel terpisah di Vercel Abang
 if (!getApps().length) {
     initializeApp({
-        credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY))
+        credential: cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        })
     });
 }
 const db = getFirestore();
